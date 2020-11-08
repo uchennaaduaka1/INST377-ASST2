611b39f3cb047857c64899b50d84657d9ce0a587
Uchenna Aduaka <uchennaaduaka4@gmaill.com>
2020-11-08 18:40:14 -0500

    test4

--- public/script.js
+++ public/script.js
@@ -1,60 +1,56 @@
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
 const restaurants = [];
 
-fetch('/api', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then((fromServer) => fromServer.json())
-    .then((jsonFromServer) => restaurants.push(...jsonFromServer))
-    .catch((err) => {
-      console.log(err);
-    });
-
-
-function findMatches(wordMatch, restaurants) {
-    if(wordMatch != ''){
-        result = restaurants.filter(curr => {
-            const regex = new RegExp(wordMatch, 'gi');
-
-            console.log(curr.name);
-            return curr.name.match(regex) 
-                || curr.category.match(regex) 
-                || curr.address_line_1.match(regex)
-                || curr.city && curr.city.match(regex)
-                || curr.zip.match(regex);
-        });
-        return result;
-    }
-    else {
-        return [];
-    }
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => restaurants.push(...data))
+  .catch((err) => {
+    console.log(err);
+  });
+  
+function findMatches(wordToMatch, restaurants) {
+  return restaurants.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.name.match(regex) || place.category.match(regex)
+  });
 }
 
-const query = document.querySelector('#search');
-
-function showMatches() {
-    $('#filteredCases').empty();
-    $('#filteredCases').append(`<ul class="suggestions"></ul>`);
-
-
-    const matchList = findMatches(this.value, restaurants);
-    const html = matchList.map(restaurant => {
-        return `
-        <li>
-            <span class="name">${restaurant.name}</span><br>
-            <span class="category">${restaurant.category}</span><br>
-            <address class="address">
-                ${restaurant.address_line_1}<br>
-                ${restaurant.city}<br>
-                ${restaurant.zip}
-            </address>
-        </li><br>
-        `;
-    }).join('');
-    $('.suggestions').append(html);
-    
+function displayMatches() {
+  if (this.value == ''){
+    suggestions.innerHTML = '';
+  } else{
+  const matchArray = findMatches(this.value, restaurants);
+  const html = matchArray.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const restaurantName = place.name.replace(regex, `<span class="hl">${this.value.toUpperCase()}</span>`);
+    const restaurantCategory = place.category.replace(regex, `<span class="hl">${this.value}</span>`);
+    const restaurantAddress = place.address_line_1;
+    const restaurantCity = place.city;
+    const restaurantZip = place.zip;
+    return `
+      <li>
+      <div class="listItem">
+        <span class="name">${restaurantName}</span>
+        <br>
+        <span class='category'>${restaurantCategory}</span>
+        <br>
+        <span class="address">${restaurantAddress}</span>
+        <br>
+        <span class="city">${restaurantCity}</span>
+        <br>
+        <span class="zip">${restaurantZip}</span>
+      </div>
+      </li>
+      <br>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
 }
 
-query.addEventListener('input', showMatches);
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
--- server.js
+++ server.js
@@ -27,10 +27,9 @@ app.route('/api')
   })
   .post(async(req, res) => {
     console.log('POST request detected');
-    console.log('Form data in res.body',req.body);
     const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
     const json = await data.json();
-    console.log('data from fetch',json);
+    console.log('fetch request data', data);
     res.json(json);
   });
 
