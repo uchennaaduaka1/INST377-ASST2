27d2ba5d546492a92909bb2a7d36bb97a5912b7d
malonniejackson <57572132+malonniejackson@users.noreply.github.com>
2020-11-08 15:52:09 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,36 +1,51 @@
-const searches = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-const restResults = [];
+const api = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-fetch(searches).then(blob => blob.json()).then(data => restResults.push(...data));
- 
-function getMatches(matchedWords,restResults) {
-    return restResults.filter(establishments => {
-        const regexMatches = new RegExp(matchedWords,'gi');
-        return establishments.name.match(regexMatches) || establishments.category.match(regexMatches)
+const restArr = [];
+
+fetch(api)
+    .then(blob => blob.json())
+    .then(data => restArr.push(...data));
+
+function filterRes(wordToMatch, restArr) {
+    return restArr.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.category.match(regex) || place.zip.match(regex)
     });
 }
+const searchInput = document.querySelector('.UserInput');
+const suggestions = document.querySelector('.suggestions')
 
-function showMatches() {
-    const matchArr = getMatches(this.value, restResults);
-    const html = matchArr.map(establishments => {
-        return `
-         <li>
-            <span class="name">${establishments.name}</span><br>
-            <span class="category">${establishments.category}</span><br>
-            <span class="address"><address>${establishments.address_line_1}, ${establishments.address_line_2}, ${establishments.city}, ${establishments.state}, ${establishments.zip}
-            </address></span>
-        </li>
-        `;
+function matches() {
+    const matchArr = filterRes(this.value, restArr);
+    const inputData = matchArr.map(place => {
+        if (this.value != '') {
+            return `
+            <li class="filteredDisplay">
+                <ul>
+                    <li>
+                        <span class="name">${place.name}</span>
+                    </li>
+                    <li>
+                        <span class="category">${place.category}</span>
+                    </li>
+                    <li>
+                        <address class="address">${place.address_line_1}</address>
+                    </li>
+                    <li>
+                        <span class="city">${place.city}</span>
+                    </li>
+                    <li>
+                        <span class="zip">${place.zip}</span>
+                        <br>
+                    </li>
+                </ul>
+            </li>
+            `;
+        }
     }).join('');
-    suggestions.innerHTML=html;
     
-    const replacedRegex = new RegExp("------,",'gi');
-    deleteEmptyAddresses = html.replace(replacedRegex," ");
-    suggestions.innerHTML=deleteEmptyAddresses;  
+    searchInput.innerHTML = inputData;
+    suggestions.innerHTML = inputData;
 }
 
-const userInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
-
-userInput.addEventListener('change',showMatches);
-userInput.addEventListener('keyup', showMatches);
+searchInput.addEventListener('keyup', matches);
